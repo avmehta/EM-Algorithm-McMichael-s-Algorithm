aec0b095360b11b2d83b20d36bcd883da8cf2063
Michael Mezher <mezhermikey@yahoo.com>
2017-08-02 16:57:57 -0400

    EM_Algorithm with iteration measured RMSE
    EM_Algorithm with iteration measured RMSE

--- EM_Algo.m
+++ EM_Algo.m
@@ -12,7 +12,7 @@ clear all
 %movies, and column 3 being different users. The data here is sorted in
 %order of user. 
 Y=csvread('Y.csv');  
-X=csvread('P.csv');
+P=csvread('P.csv');
 
 %%Creating the appropriate matrices
 %First we'll get our Z matrix (from the data in Y). This puts what Y was
@@ -24,6 +24,11 @@ Z=getZmat(Y);
 ZN=getZmatNaN(Y);
 rtings=size(Z,1);
 users=size(Z,2);
+
+%Creating matrix to check RMSE/MMSE (These are essentially target values)
+P=getZmat(P);
+
+
 %mu is the mean vector corresponding to all user ratings in Z(sub t) It is
 %transposed to keep the dimensions consistent (after transposing Z2). 
 %As is defined in the paper, mu_yt are simply subsets of this mean vector
@@ -45,8 +50,10 @@ R=id_mat;
 %Number of iterations to perform EM algorithm. Note that in practice, the
 %algorithm will continue iterations until some convergence condition is
 %met. 
-iter=27; %27 is what it took to converge in Robert's paper
-
+iter=100; %27 is the number of iters EM took to converge in Robert's paper
+lastIter=0; 
+%A vector of RMSEs from each iteration
+RMSEs=zeros(1,1);
 %We will loop every iteration, updating mu, then subsequently updating R.
 for ii = 1:iter
     %Updating mu 
@@ -70,11 +77,15 @@ for ii = 1:iter
     mu_hat=inv(sumTerm1)*sumTerm2;
 
     
-    %Updating R
-    sumTerm3=zeros(rtings,rtings); %0 the summation terms. 
+    %Updating R and calculating error
+    sumTerm3=zeros(rtings,rtings); %0 the summation terms. (and initialize)
     sumTerm4=zeros(rtings,rtings);
+    totalLen=0;
+    sum_t_terms=0;
+    Last_RMSE=5;
     for usrIdx=1:size(Z,2)
         usr=Z(:,usrIdx);
+        usrT=P(:,usrIdx); %Targets of user
         H_yt=getHyt(usr,id_mat);
         %H_xt is an subset of the identity matrix corresponding to only
         %unobserved values. 
@@ -96,12 +107,34 @@ for ii = 1:iter
         term2=H_xt'*(R_xt-(R_xtyt*(inv(R_yt))*((R_xtyt)')))*H_xt;
         sumTerm3=term1+sumTerm3;
         sumTerm4=term2+sumTerm4;
+        %%Gather RMSE/MMSE
+        %Below replicates what is described in Section 3 subsection B Eq 12
+        H_ytT=getHyt(usrT,id_mat); %Observed TARGET values*
+        specP=H_ytT*Z_hat_t; %Specific values corresponding to target vals
+        yt_T=H_ytT*usrT; %Target vals of this user. Cmpare agnst pred vals
+        lt=size(H_ytT,1); %Number of classifications made this iteration
+        t_terms=yt_T-specP; %Top terms (Equation 12)
+        totalLen=lt+totalLen;
+        sum_t_terms=sum_t_terms+((t_terms)'*(t_terms));
     end
+    %Calculating this iteration's error
+    errSq=sum_t_terms./totalLen;
+    RMSE=sqrt(errSq);
     %Calculating next iteration's covariance
-    R=(sumTerm3+sumTerm4)./(size(Z,2)); %Where size(Z,2) is n. **May be ./**
+    R=(sumTerm3+sumTerm4)./(size(Z,2)); %Where size(Z,2) is n
+    
+    %Check if convergence criterion was met. If it was, stop iterating.
+    %Note that this convergence criterion is not exactly as described in
+    %equation 13, however it is similar and accomplishes the same purpose.
+    RMSEs=vertcat(RMSEs,RMSE);
+    if Last_RMSE-RMSE<=.0005 %Convergence condition
+        MMSE=RMSE; %Lowest RMSE
+        break; 
+    end
+    LastRMSE=RMSE;
 end
+RMSEs(1,1)=[]; %Clear zero row;
 
-%%Next we need to calculate the RMSE of the predicted values. 
 
 
 
